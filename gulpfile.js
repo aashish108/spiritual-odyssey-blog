168d47cc2de4c21f72ea6d6d95487c65ecf6d8da
Aashish Sharma <aashish.sharma.108@gmail.com>
2020-04-19 17:49:08 +0100

    Redid the styles, worked on gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const uglifycss = require('gulp-uglifycss');
 
 const { series, parallel } = require('gulp');
 
-gulp.task('scss', function () {
+gulp.task('sass', function () {
   return gulp.src('./assets/css/scss/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(uglifycss({
+      "uglyComments": true
+    }))
     .pipe(gulp.dest('./assets/css'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./assets/css/scss/**/*.scss', ['scss']);
-});
 
-exports.build = series(sass);
+gulp.task('default', gulp.series('sass:watch'));

73e470509fd43a65ef784a7f01ab42c42480a7ec
Aashish Sharma <aashish.sharma.108@gmail.com>
2020-04-22 20:25:12 +0100

    More Gulp work; content work

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 const uglifycss = require('gulp-uglifycss');
+const del = require('del');
 
 const { series, parallel } = require('gulp');
 
@@ -22,6 +23,15 @@ gulp.task('sass:watch', function() {
   gulp.watch('./assets/css/scss/**/*.scss', gulp.series('sass'));
 });
 
+// Task to delete target build folder
+gulp.task('clean:prd', function () {
+  return del([
+    'delete-report.csv',
+    // here we use a globbing pattern to match everything inside the `spiritualodyssey` folder
+    '../../../../../../../../mnt/share/Cloud/personal-blogs/spiritualodyssey/**/*',
+  ]);
+});
+
 // The below is for use on my local Pi device that mounts my Synology Cloud share folder
 gulp.task('copyToVirtualHostDIR', function() {
   return gulp.src('_site/**/*', {base:'_site/'})
@@ -30,4 +40,4 @@ gulp.task('copyToVirtualHostDIR', function() {
 
 gulp.task('sass', gulp.series('sass'));
 gulp.task('sass-watch', gulp.series('sass', 'sass:watch'));
-gulp.task('copyToPrd', gulp.series('copyToVirtualHostDIR'));
+gulp.task('copyToPrd', gulp.series('clean:prd', 'copyToVirtualHostDIR'));
